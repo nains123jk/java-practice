9d593d60ebdf838e8efd28a0a67148e310715b18
Naina <nainajunnake143@gmail.com>
2024-05-13 09:22:28 +0530

    addition program

--- practice/Addition.java
+++ practice/Addition.java
@@ -1,4 +1,10 @@
 package practice;
 
 public class Addition {
+    public static void main(String[] args) {
+int first = 10;
+int second = 30;
+ int sum = first + second;
+        System.out.println(first+"+"+second+"="+sum);
+    }
 }
