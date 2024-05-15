7052d3e2936e55ec521118a516cc1f45b4a58808
Naina <nainajunnake143@gmail.com>
2024-05-15 09:08:54 +0530

    addition program

---
+++ practice/Forloop1to100.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class Forloop1to100 {
+    public static void main(String[] args) {
+        int n = 100;
+        System.out.print("Numbers from 1 to "+n+" are: ");
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
---
+++ practice/Odd1to100for.java
@@ -0,0 +1,14 @@
+package practice;
+public class Odd1to100for {
+    public static void main(String[] args) {
+        int num=100;
+        System.out.print("List of even numbers from 1 to "+num+": ");
+        for (int i=1; i<=num; i++)
+        {
+            if (i%2!=0)
+            {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
