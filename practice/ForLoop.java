72cd0a161cb01f4d8659e821faf42948d6c808c4
Naina <nainajunnake143@gmail.com>
2024-05-20 13:25:39 +0530

    Program Calculate Sum of Number using For Loop .

---
+++ practice/ForLoop.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int a = 1; a <= 50;a++)
+        {
+            sum = sum + a;
+        }
+        System.out.println("Sum:"+sum);
+    }
+}
