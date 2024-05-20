e4433642d3facad912b9eb912b722550eca39636
Naina <nainajunnake143@gmail.com>
2024-05-20 13:29:53 +0530

    If Statement Program.

---
+++ practice/IfStatment.java
@@ -0,0 +1,9 @@
+package practice;
+
+public class IfStatment {
+    public static void main(String[] args) {
+        int i = 20;
+        if (i < 15);
+        System.out.println("20 is Greater than 15 ");
+    }
+}
