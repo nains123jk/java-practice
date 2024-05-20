d50e7eaf78f4412342d38f17fbdd95d722bb0c85
Naina <nainajunnake143@gmail.com>
2024-05-20 13:38:58 +0530

    WhileLoop Statement program.

---
+++ practice/WhileLoop.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int i = 1 , n = 20;
+        while (i<=n)
+        {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
