f4f50e933e2b3a8c4aaf02ec40b1f634b0ac4488
Naina <nainajunnake143@gmail.com>
2024-05-22 17:03:51 +0530

    Addition of Three Numbers

---
+++ practice/ThreeNumSum.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class ThreeNumSum {
+    public static void main(String[] args) {
+        int i = ThreeNumAdd(10,20, 30);
+        System.out.println(i);
+    }
+    public static int ThreeNumAdd(int a, int b, int c) {
+        int sum = a + b + c;
+        return sum;
+    }
+}
